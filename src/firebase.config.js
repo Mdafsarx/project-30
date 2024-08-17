facbb1315fb8875ed14285e14d309c08046487d4
Mdafsarx <mdafsar99009@gmail.com>
2024-08-17 16:21:18 +0600

    update

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAP8qW82N7pMvamstWwbvw6AIa4Z09x6So",
-  authDomain: "project-30-b2e7b.firebaseapp.com",
-  projectId: "project-30-b2e7b",
-  storageBucket: "project-30-b2e7b.appspot.com",
-  messagingSenderId: "10464464234",
-  appId: "1:10464464234:web:6807351867959987b5e913"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
